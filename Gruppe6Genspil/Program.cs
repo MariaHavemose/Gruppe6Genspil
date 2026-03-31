b0bf3944e820da6913536d5ccecf8458b13a5c43
MariaHavemose <mariahavemose@proton.me>
2026-03-31 12:34:17 +0200

    Add project files.

---
+++ Gruppe6Genspil/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Gruppe6Genspil
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
