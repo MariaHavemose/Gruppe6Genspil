ae23d9c5edb053278860b642d4448ea88c1c5367
MariaHavemose <mariahavemose@proton.me>
2026-04-01 10:36:28 +0200

    Game klasse done (fra min ende)

--- Gruppe6Genspil/Game.cs
+++ Gruppe6Genspil/Game.cs
@@ -18,5 +18,26 @@ namespace Gruppe6Genspil
 
         public List<GameCopy> Copies { get; set; } = new List<GameCopy>();
 
+        public Game(string name, string genre, int maxPlayers, int minPlayers, int ageRating)
+        {
+            Name = name;
+            Genre = genre;
+            MaxPlayers = maxPlayers;
+            MinPlayers = minPlayers;
+            AgeRating = ageRating;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name},{Genre},{MaxPlayers},{MinPlayers},{AgeRating}";
+
+        }
+
+        public static Game FromString(string data)
+        {
+            string[] parts = data.Split(',');
+            return new Game(parts[0], parts[1], int.Parse(parts[2]), int.Parse(parts[3]), int.Parse(parts[4]));
+        }
     }
+
 }
