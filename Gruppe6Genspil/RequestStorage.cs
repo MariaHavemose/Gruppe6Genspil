46bf60c6f8393a7ecec37a978adfe79ba6ade3f1
MariaHavemose <mariahavemose@proton.me>
2026-04-01 12:30:02 +0200

    Tilføjet database tekst dokument

---
+++ Gruppe6Genspil/RequestStorage.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gruppe6Genspil
+{
+    internal class RequestStorage
+    {
+        public string FilePath { get; set; }
+
+        public RequestStorage(string filePath)
+        {
+            FilePath = filePath;
+        }
+
+        public void SaveRequestToFile(List<Request> requests)
+        {
+            using (StreamWriter sw = new StreamWriter(FilePath))
+            {
+                foreach (var request in requests)
+                {
+                    sw.WriteLine(request.ToString());
+                }
+            }
+        }
+
+        public List<Request> LoadRequestFromFile()
+        {
+            List<Request> requests = new List<Request>();
+
+            using (StreamReader sr = new StreamReader(FilePath))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    requests.Add(Request.FromString(line));
+                }
+            }
+
+            return requests;
+        }
+    }
+
+
+
+}
+
+
+
