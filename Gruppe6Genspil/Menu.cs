809d2f4d16c0624d01113fc4ea2b08b4d72d9558
SannaKazmouz <smlu79153@edu.ucl.dk>
2026-04-16 12:20:18 +0200

    Fjernet tomme linjer i Menu.

--- Gruppe6Genspil/Menu.cs
+++ Gruppe6Genspil/Menu.cs
@@ -3,30 +3,21 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-// MENU-KLASSEN: Håndterer brugerens input og viser menuen
 public class Menu
 {
-    // Reference til Storage (lager-logikken)
     private Storage _storage;
     private RequestStorage _requestStorage;
-
-    // Konstruktør: menuen skal bruge Storage
     public Menu(Storage storage, RequestStorage requestStorage)
     {
         _storage = storage;
         _requestStorage = requestStorage;
     }
-
-    // Hovedmenuen som kører i en løkke
     public void Start()
     {
         bool running = true;
-
         while (running)
         {
             Console.Clear();
-
-            // Viser menuvalg til brugeren
             Console.WriteLine("=== Lagerstyringssystem ===");
             Console.WriteLine("1. Vis lager");
             Console.WriteLine("2. Søg efter spil");
@@ -35,37 +26,28 @@ public class Menu
             Console.WriteLine("5. Vis forespørgsler");
             Console.WriteLine("6. Afslut program");
             Console.Write("Vælg menu punkt: ");
-
             string choice = Console.ReadLine();
             Console.Clear();
-
-            // Behandler brugerens valg
             switch (choice)
             {
                 case "1":
                     _storage.WriteAllGames();
                     break;
-
                 case "2":
                     SearchGameMenu();
                     break;
-
                 case "3":
                     AddGameMenu();
                     break;
-
                 case "4":
                     AddRequestMenu();
                     break;
-
                 case "5":
                     _requestStorage.ShowRequests();
                     break;
-
                 case "6":
                     running = false;
                     break;
-
                 default:
                     Console.WriteLine("Ugyldigt valg!");
                     break;
@@ -74,43 +56,30 @@ public class Menu
             Console.ReadKey();
         }
     }
-
-    // Menu punkt: søg efter spil
     private void SearchGameMenu()
     {
         Console.WriteLine("=== Søg efter spil ===");
-
         SearchCriteria criteria = new SearchCriteria();
-
         Console.Write("Navn (eller del af navn): ");
         criteria.Name = Console.ReadLine();
-
         Console.Write("Genre: ");
         criteria.Genre = Console.ReadLine();
-
         Console.Write("Stand: ");
         criteria.Condition = Console.ReadLine();
-
         Console.Write("Minpris: ");
         if (double.TryParse(Console.ReadLine(), out double minP))
             criteria.MinPrice = minP;
-
         Console.Write("Maxpris: ");
         if (double.TryParse(Console.ReadLine(), out double maxP))
             criteria.MaxPrice = maxP;
-
         Console.Write("Minimum spillere: ");
         if (int.TryParse(Console.ReadLine(), out int minPl))
             criteria.MinPlayers = minPl;
-
         Console.Write("Maksimum spillere: ");
         if (int.TryParse(Console.ReadLine(), out int maxPl))
             criteria.MaxPlayers = maxPl;
-
         var results = _storage.SearchGame(criteria);
-
         Console.WriteLine("\n--- Resultat ---\n");
-
         if (results.Count == 0)
         {
             Console.WriteLine("Ingen spil fundet.");
@@ -123,33 +92,23 @@ public class Menu
             }
         }
     }
-
-    // Menu punkt: Tilføj et nyt spil
     private void AddGameMenu()
     {
         Console.Write("Spil navn: ");
         string name = Console.ReadLine();
-
         Console.Write("Genre: ");
         string genre = Console.ReadLine();
-
         Console.Write("Maks spillere: ");
         int maxplayers = int.Parse(Console.ReadLine());
-
         Console.Write("Min spillere: ");
         int minplayers = int.Parse(Console.ReadLine());
-
         Console.Write("Aldersgrænse: ");
         int ageRating = int.Parse(Console.ReadLine());
-
         Console.Write("Variant: ");
         string variant = Console.ReadLine();
-
         Game game = new Game(name, genre, maxplayers, minplayers, ageRating, variant);
         _storage.AddGame(game);
     }
-
-    // Menu punkt: Tilføj en kundeforespørgsel
     private void AddRequestMenu()
     {
         Console.Write("Kundens navn: ");
