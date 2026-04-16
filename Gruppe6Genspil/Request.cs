e931f5c23a3308935e847c40179d77982cfb5e0e
SannaKazmouz <smlu79153@edu.ucl.dk>
2026-04-16 12:21:04 +0200

    Fjernet tomme linjer i Request.

--- Gruppe6Genspil/Request.cs
+++ Gruppe6Genspil/Request.cs
@@ -4,22 +4,17 @@ using System.Text;
 
 namespace Gruppe6Genspil
 {
-    // REQUEST-KLASSEN (Forespørgsel fra kunde)
     public class Request
     {
         public string CustomerName { get; set; }
         public string GameName { get; set; }
         public string Comment { get; set; }
-
-        // KONSTRUKTØR (Laver et nyt Request-objekt)
         public Request(string customerName, string gameName, string comment)
         {
             CustomerName = customerName;
             GameName = gameName;
             Comment = comment;
         }
-
-        // VISER REQUEST SOM TEKST
         public override string ToString()
         {
             return $"{CustomerName},{GameName},{Comment}";
